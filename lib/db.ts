3a97ba6d7275dbcef9df768e9d2d5c270cf14498
gpalakgupta <palakgupta5709@gmail.com>
2025-09-09 00:48:17 +0530

    database connected

---
+++ lib/db.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { buffer } from "stream/consumers";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+        bufferCommands:true,
+        maxPoolSize:10,
+    };
+    mongoose.connect(MONGODB_URI,opts).then(() => mongoose.connect);
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
+  return cached.conn;
+}
